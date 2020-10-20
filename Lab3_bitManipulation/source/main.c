23bcf2cab50af57380f7514426a690779f9e0930
Luke <lmcfa003@ucr.edu>
2020-10-19 23:49:26 -0700

    Fixing merge conflicts

--- Lab3_bitManipulation/source/main.c
+++ Lab3_bitManipulation/source/main.c
@@ -1,10 +1,10 @@
-/*	Author: Luke McFadden
- *	Lab Section: 023
- *	Assignment: Lab 3  Exercise 1
- *	Exercise Description: [optional - include for your own benefit]
+/*  Author: Luke McFadden
+ *  Lab Section: 023
+ *  Assignment: Lab 3  Exercise 1
+ *  Exercise Description: [optional - include for your own benefit]
  *
- *	I acknowledge all content contained herein, excluding template or example
- *	code, is my own original work.
+ *  I acknowledge all content contained herein, excluding template or example
+ *  code, is my own original work.
  */
 
 #include <avr/io.h>
@@ -15,27 +15,27 @@
 int main(void) 
 {
     /* Insert DDR and PORT initializations */
-	DDRA = 0x00; PORTA = 0xFF; // Configure port A's 8 pins as inputs
-	DDRB = 0x00; PORTB = 0xFF; // Configure port B's 8 pins as inputs
-	DDRC = 0xFF; PORTC = 0x00; // Configure port B's 8 pins as outputs, initialize to 0s
-	unsigned char tempA = 0x00;
-	unsigned char tempB = 0x00;
-	unsigned char counter = 0x00;
+    DDRA = 0x00; PORTA = 0xFF; // Configure port A's 8 pins as inputs
+    DDRB = 0x00; PORTB = 0xFF; // Configure port B's 8 pins as inputs
+    DDRC = 0xFF; PORTC = 0x00; // Configure port B's 8 pins as outputs, initialize to 0s
+    unsigned char tempA = 0x00;
+    unsigned char tempB = 0x00;
+    unsigned char counter = 0x00;
 
 
     /* Insert your solution below */
     while (1) 
     {
-    	// Set temp variables to PINs A and B
-    	tempA = PINA;
-    	tempB = PINB;
+        // Set temp variables to PINs A and B
+        tempA = PINA;
+        tempB = PINB;
 
-    	// Count and add amount of 1's within PINs A and B
-    	counter = ((tempA & 0x80) >> 7) + ((tempA & 0x40) >> 6) + ((tempA & 0x20) >> 5) + ((tempA & 0x10) >> 4) + ((tempA & 0x08) >> 3) + ((tempA & 0x04) >> 2) + ((tempA & 0x02) >> 1) + (tempA & 0x01); 
-    	counter = counter + ((tempB & 0x80) >> 7) + ((tempB & 0x40) >> 6) + ((tempB & 0x20) >> 5) + ((tempB & 0x10) >> 4) + ((tempB & 0x08) >> 3) + ((tempB & 0x04) >> 2) + ((tempB & 0x02) >> 1) + (tempB & 0x01); 
-    	
-    	// Give PORT C total amount of 1's
-    	PORTC = counter;
+        // Count and add amount of 1's within PINs A and B
+        counter = ((tempA & 0x80) >> 7) + ((tempA & 0x40) >> 6) + ((tempA & 0x20) >> 5) + ((tempA & 0x10) >> 4) + ((tempA & 0x08) >> 3) + ((tempA & 0x04) >> 2) + ((tempA & 0x02) >> 1) + (tempA & 0x01); 
+        counter = counter + ((tempB & 0x80) >> 7) + ((tempB & 0x40) >> 6) + ((tempB & 0x20) >> 5) + ((tempB & 0x10) >> 4) + ((tempB & 0x08) >> 3) + ((tempB & 0x04) >> 2) + ((tempB & 0x02) >> 1) + (tempB & 0x01); 
+        
+        // Give PORT C total amount of 1's
+        PORTC = counter;
     }
 
     return 1;
--- Lab3_bitManipulation/test/tests.py
+++ Lab3_bitManipulation/test/tests.py
@@ -15,34 +15,41 @@
 # that they are not ran in isolation but in the order shown and the state of the device is not reset or 
 # altered in between executions (unless preconditions are used).
 tests = [ 
+
+    # Test counting amount of 1's in PINA and PINB
+
     {'description': 'PINA[1:0]: 00, PINB[1:0]: 00',
-    'steps': [ {'inputs': [('PINA',0x00), ('PINB', 0x00)], 'iterations': 2 } ],
+    'steps': [ {'inputs': [('PINA',0x00), ('PINB',0x00)], 'iterations': 2 } ],
     'expected': [('PORTC',0x00)]
     },
-    {'description': 'PINA[1:0]: FF, PINB[1:0]: 00',
-    'steps': [ {'inputs': [('PINA',0xFF), ('PINB', 0x00)], 'iterations': 2 } ],
+    {'description': 'PINA[1:0]: FF, PINB[1:0]: FF',
+    'steps': [ {'inputs': [('PINA',0xFF), ('PINB',0xFF)], 'iterations': 2 } ],
+    'expected': [('PORTC',0x10)]
+    },
+    {'description': 'PINA[1:0]: F0, PINB[1:0]: 0F',
+    'steps': [ {'inputs': [('PINA',0xF0), ('PINB',0x0F)], 'iterations': 2 } ],
     'expected': [('PORTC',0x08)]
     },
-    {'description': 'PINA[1:0]: 00, PINB[1:0]: FF',
-    'steps': [ {'inputs': [('PINA',0x00), ('PINB', 0xFF)], 'iterations': 2 } ],
+    {'description': 'PINA[1:0]: 12, PINB[1:0]: 34',
+    'steps': [ {'inputs': [('PINA',0x12), ('PINB',0x34)], 'iterations': 2 } ],
+    'expected': [('PORTC',0x05)]
+    },
+    {'description': 'PINA[1:0]: 56, PINB[1:0]: 78',
+    'steps': [ {'inputs': [('PINA',0x56), ('PINB',0x78)], 'iterations': 2 } ],
     'expected': [('PORTC',0x08)]
     },
-    {'description': 'PINA[1:0]: 07, PINB[1:0]: 70',
-    'steps': [ {'inputs': [('PINA',0x70), ('PINB', 0x07)], 'iterations': 2 } ],
-    'expected': [('PORTC',0x06)]
-    },
-    {'description': 'PINA[1:0]: A1, PINB[1:0]: C4',
-    'steps': [ {'inputs': [('PINA',0xA1), ('PINB', 0xC4)], 'iterations': 2 } ],
-    'expected': [('PORTC',0x06)]
-    },
-    {'description': 'PINA[1:0]: BF, PINB[1:0]: 42',
-    'steps': [ {'inputs': [('PINA',0xBF), ('PINB', 0x42)], 'iterations': 2 } ],
+    {'description': 'PINA[1:0]: 9A, PINB[1:0]: BC',
+    'steps': [ {'inputs': [('PINA',0x9A), ('PINB',0xBC)], 'iterations': 2 } ],
     'expected': [('PORTC',0x09)]
     },
+    {'description': 'PINA[1:0]: A1, PINB[1:0]: C4',
+    'steps': [ {'inputs': [('PINA',0xA1), ('PINB',0xC4)], 'iterations': 2 } ],
+    'expected': [('PORTC',0x06)]
+    },
+
     ]
 
 # Optionally you can add a set of "watch" variables these need to be global or static and may need
 # to be scoped at the function level (for static variables) if there are naming conflicts. The 
 # variables listed here will display everytime you hit (and stop at) a breakpoint
 watch = ['main::tempA, tempB','PORTC']
-
