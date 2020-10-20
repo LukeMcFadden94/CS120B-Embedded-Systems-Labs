0713534dd4f234b8d521ce057a2e3aaeda68f83b
Luke <lmcfa003@ucr.edu>
2020-10-19 23:24:35 -0700

    Fixed issues with .py test files

--- turnin/lmcfa003_lab2_part1_tests.py
+++ turnin/lmcfa003_lab2_part1_tests.py
@@ -1,46 +1,43 @@
-# Test file for "Lab2_introToAVR"
+# Array of tests to run (in order)
+# Each test contains
+#   description - 
+#   steps - A list of steps to perform, each step can have
+#       inputs - A list of tuples for the inputs to apply at that step
+#       *time - The time (in ms) to wait before continuing to the next step 
+#           and before checking expected values for this step. The time should be a multiple of
+#           the period of the system
+#       *iterations - The number of clock ticks to wait (periods)
+#       expected - The expected value at the end of this step (after the "time" has elapsed.) 
+#           If this value is incorrect the test will fail early before completing.
+#       * only one of these should be used
+#   expected - The expected output (as a list of tuples) at the end of this test
+# An example set of tests is shown below. It is important to note that these tests are not "unit tests" in 
+# that they are not ran in isolation but in the order shown and the state of the device is not reset or 
+# altered in between executions (unless preconditions are used).
+tests = [ 
 
+    # Test for LED light output, inputs 0x00 through 0x03
 
-# commands.gdb provides the following functions for ease:
-#   test "<message>"
-#       Where <message> is the message to print. Must call this at the beginning of every test
-#       Example: test "PINA: 0x00 => expect PORTC: 0x01"
-#   checkResult
-#       Verify if the test passed or failed. Prints "passed." or "failed." accordingly, 
-#       Must call this at the end of every test.
-#   expectPORTx <val>
-#       With x as the port (A,B,C,D)
-#       The value the port is epected to have. If not it will print the erroneous actual value
-#   setPINx <val>
-#       With x as the port or pin (A,B,C,D)
-#       The value to set the pin to (can be decimal or hexidecimal
-#       Example: setPINA 0x01
-#   printPORTx f OR printPINx f 
-#       With x as the port or pin (A,B,C,D)
-#       With f as a format option which can be: [d] decimal, [x] hexadecmial (default), [t] binary 
-#       Example: printPORTC d
-#   printDDRx
-#       With x as the DDR (A,B,C,D)
-#       Example: printDDRB
+    {'description': 'PINA[1:0]: 00',
+    'steps': [ {'inputs': [('PINA',0x00)], 'iterations': 2 } ],
+    'expected': [('PORTB',0x00)]
+    },
+    {'description': 'PINA[1:0]: 01',
+    'steps': [ {'inputs': [('PINA',0x01)], 'iterations': 2 } ],
+    'expected': [('PORTB',0x01)]
+    },
+    {'description': 'PINA[1:0]: 02',
+    'steps': [ {'inputs': [('PINA',0x02)], 'iterations': 2 } ],
+    'expected': [('PORTB',0x00)]
+    },
+    {'description': 'PINA[1:0]: 03',
+    'steps': [ {'inputs': [('PINA',0x03)], 'iterations': 2 } ],
+    'expected': [('PORTB',0x00)]
+    },
 
-echo ======================================================\n
-echo Running all tests..."\n\n
+]
 
-# Example test:
-test "PINA: 0x00, PINB: 0x00 => PORTC: 0"
-# Set inputs
-setPINA 0x00
-setPINB 0x00
-# Continue for several ticks
-continue 2
-# Set expect values
-expectPORTC 0
-# Check pass/fail
-checkResult
-
-# Add tests below
-
-# Report on how many tests passed/tests ran
-set $passed=$tests-$failed
-eval "shell echo Passed %d/%d tests.\n",$passed,$tests
-echo ======================================================\n
+# Optionally you can add a set of "watch" variables these need to be global or static and may need
+# to be scoped at the function level (for static variables) if there are naming conflicts. The 
+# variables listed here will display everytime you hit (and stop at) a breakpoint
+watch = ['main::tempA','PORTB']
