ffd92f8a1284893680afd51b55d8b6bbb72bab4f
Luke <lmcfa003@ucr.edu>
2020-11-08 23:45:34 -0800

    Finished Lab 7

--- Lab7_ADC/turnin/lmcfa003_lab7_part3.c
+++ Lab7_ADC/turnin/lmcfa003_lab7_part3.c
@@ -1,13 +1,13 @@
 /*	Author: Luke McFadden
  *  Partner(s) Name: 
  *	Lab Section: 023
- *	Assignment: Lab 7  Exercise 4
+ *	Assignment: Lab 7  Exercise 3
  *	Exercise Description: [optional - include for your own benefit]
  *
  *	I acknowledge all content contained herein, excluding template or example
  *	code, is my own original work.
  *
- *  Demo Link: https://youtu.be/HBKbhn9mrEg
+ *  Demo Link: https://youtu.be/nBqd14MGoaA
  */
 #include <avr/io.h>
 #ifdef _SIMULATE_
@@ -23,8 +23,9 @@ int main(void)
 {
     DDRA = 0x00; PORTA = 0xFF;
     DDRB = 0xFF; PORTB = 0x00;
+    DDRD = 0xFF; PORTD = 0x00;
 
-    unsigned short max = 128;
+    unsigned short max = 127;
     unsigned char light = 0x00;
 
     ADC_init();
@@ -34,24 +35,12 @@ int main(void)
     {
         unsigned short x = ADC;
 
-        if (x >= max)
-            light = 0xFF;
-        else if (x >= 112)
-            light = 0x7F;
-        else if (x >= 96)
-            light = 0x3F;
-        else if (x >= 80)
-            light = 0x1F;
-        else if (x >= 64)
-            light = 0x0F;
-        else if (x >= 48)
-            light = 0x07;
-        else if (x >= 32)
-            light = 0x03;
-        else if (x >= 16)
-            light = 0x01;
-        else if (x <= 0)
-            light = 0x00;
+        if (x > max)
+            max = x;
+        else if (x >= max/2)
+            light = 1;
+        else
+            light = 0;
 
         PORTB = light;
     }
