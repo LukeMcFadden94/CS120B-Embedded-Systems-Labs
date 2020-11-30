018079ec0a22d86c12505d13f87814cdab8912aa
Luke <lmcfa003@ucr.edu>
2020-11-29 21:50:50 -0800

    Updated part 2

--- Lab11_externalRegisters/turnin/lmcfa003_lab11_part2.c
+++ Lab11_externalRegisters/turnin/lmcfa003_lab11_part2.c
@@ -2,12 +2,12 @@
  *  Partner(s) Name: 
  *	Lab Section:
  *	Assignment: Lab 11 Exercise 2
- *	Exercise Description: https://youtu.be/UimC3f4LTZU
+ *	Exercise Description: 
  *
  *	I acknowledge all content contained herein, excluding template or example
  *	code, is my own original work.
  *
- * Demo Link: 
+ * Demo Link: https://youtu.be/UimC3f4LTZU
  */
 #include <avr/io.h>
 #ifdef _SIMULATE_
